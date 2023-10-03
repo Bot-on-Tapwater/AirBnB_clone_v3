36a6d2fb6ed40b8bf60901f62f8f19001e4745c9
raymond <stancillousray@gmail.com>
2023-10-03 14:21:47 +0300

    fixed pep8 styling errors

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -7,15 +7,19 @@ from api.v1.views import app_views
 import json
 from flask import request, jsonify, abort
 
-@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['GET'], strict_slashes=False)
 def get_cities(state_id):
     """Retrieves a list of all State objects"""
     cities = storage.all(City)
-    list_cities = [city.to_dict() for city in cities.values() if city.state_id == state_id]
+    list_cities = [city.to_dict() for city in cities.values()
+                   if city.state_id == state_id]
 
     if len(list_cities) == 0:
         abort(404)
     return jsonify(list_cities)
+
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
 def server_city_id(city_id):
@@ -26,6 +30,7 @@ def server_city_id(city_id):
         abort(404)
 
     return jsonify(response.to_dict())
+
 
 @app_views.route('/cities/<city_id>',
                  methods=['DELETE'], strict_slashes=False)
@@ -40,7 +45,9 @@ def delete_city_obj(city_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+
+@app_views.route('/states/<state_id>/cities',
+                 methods=['POST'], strict_slashes=False)
 def create_new_city(state_id):
     """creates a City"""
 
@@ -69,6 +76,7 @@ def create_new_city(state_id):
     storage.new(new_city)
     storage.save()
     return jsonify(new_city.to_dict()), 201
+
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def update_city_obj(city_id):
